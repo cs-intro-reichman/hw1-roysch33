a0eca967e6c8108cb47648ad2a6f699c1825d1b3
roy <roysch33@gmail.com>
2024-11-08 18:44:40 +0200

    demo0 back to normal

--- Demo0.java
+++ Demo0.java
@@ -1,6 +1,6 @@
 // Prints the numbers 0 to 5.
 // (Same program as shown in lecture 1-1).
-public class Print5 {
+public class Demo0 {
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 0
         int i = 0;
@@ -10,6 +10,6 @@ public class Print5 {
             System.out.println(i);
             i = i + 1;
             }
-            System.out.println("Done");
+            System.out.println("done");
     }
  }
