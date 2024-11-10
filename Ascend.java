3c8c69a01d658288d0c32c5c7fa0c6f3a2f2da42
roy <roysch33@gmail.com>
2024-11-11 00:14:29 +0200

    finish Ascend

--- Ascend.java
+++ Ascend.java
@@ -2,15 +2,21 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		// naming the args:
 		int lim = Integer.parseInt(args[0]);
+		// setting a random using the Math.random() commend and setting it to lim+1 range so we can get all the numbers from 0 to lim.
 		int a = (int)(Math.random() * (lim+1));
 		int b = (int)(Math.random() * (lim+1));
 		int c = (int)(Math.random() * (lim+1));
-		boolean smallNum = a < b < c;
-		int middleNum = Math.min(a,b);
-		int bigNum = Math.min(a,b);
+		//getting the smallest number by using the Math.min() command.
+		int smallNum = Math.min(a,b);
+		int smallestNum = Math.min(smallNum,c);
+		//getting the biggest number by using the Math.max() command.
+		int bigNum = Math.max(a, b);
+		int biggestNum = Math.max(bigNum,c);
+		// Calculating the middle number by finding the value that's not the smallest or largest
+		int middleNum = a + b + c - smallestNum - biggestNum;
 
-		System.out.println(a + " " + b + " " + c);
+		System.out.println(smallestNum + " " + middleNum + " " + biggestNum);
 	}
 }
