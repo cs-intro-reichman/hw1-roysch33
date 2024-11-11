297e551b76c972254a5f4cf06747d221c3a8d089
roy <roysch33@gmail.com>
2024-11-11 10:37:23 +0200

    finish TimeFormat final part

--- TimeFormat.java
+++ TimeFormat.java
@@ -4,30 +4,47 @@ public class TimeFormat {
 		// using the given command for getting the hours and minutes:
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-		// finding if the hours are PM - from 13 till 23:
-		if (hours > 12) {
-			hours = hours - 12;
+		// finding if the hour is 12:
+		if (hours == 12) {
 			// finding if the minutes are single digits:
 			if (minutes < 10) {
 				// if they are - adding 0 before the minutes:
-				System.out.print(hours + ":");
+				System.out.print(12 + ":");
 				System.out.print(0);
 				System.out.print(minutes + " PM");
 			}
 			else {
-				System.out.println(hours + ":" + minutes + " PM");
+				System.out.println(12 + ":" + minutes + " PM");
 			};
 		}
-		// we do the same for the minutes in the AM time:
+		// finding if the hours are PM - from 13 till 23:
 		else {
-			if (minutes < 10) {
-				System.out.print(hours + ":");
-				System.out.print(0);
-				System.out.print(minutes + " AM");
+			if (hours > 12) {
+				hours = hours - 12;
+				// finding if the minutes are single digits:
+				if (minutes < 10) {
+					// if they are - adding 0 before the minutes:
+					System.out.print(hours + ":");
+					System.out.print(0);
+					System.out.print(minutes + " PM");
+				}
+				else {
+					System.out.println(hours + ":" + minutes + " PM");
+				};
 			}
+			// we do the same for the minutes in the AM time:
 			else {
-				System.out.println(hours + ":" + minutes + " AM");
-			};
+				// finding if the minutes are single digits:
+				if (minutes < 10) {
+					// if they are - adding 0 before the minutes:
+					System.out.print(hours + ":");
+					System.out.print(0);
+					System.out.print(minutes + " AM");
+				}
+				else {
+					System.out.println(hours + ":" + minutes + " AM");
+				};
+			}
 		}
 	}
 }
